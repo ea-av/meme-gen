fa1f3cb099e561c806b155c30720178dffa66d65
ido tirem <idotirem@gmail.com>
2017-11-18 21:44:51 +0200

    מוצ"ש תיקוני עיצובים

--- js/edit-meme.js
+++ js/edit-meme.js
@@ -2,19 +2,23 @@
 
 console.log('edit-meme');
 
-function openEditMod(elImg){
+function openEditMod(elImg) {
     var elEditArea = document.querySelector('.edit-area');
     var elEditAreaImg = document.querySelector('.edit-area-img');
-        elEditArea.style.display = 'block';
-        console.log(elImg.id);
-        console.log(elImg.src);
-        elEditAreaImg.src = elImg.src;
-        // elEditAreaImg.style.margin = 20 px;
+    elEditArea.style.display = 'block';
+    var elImgArea = document.querySelector('.imgs-container');
+    elImgArea.style.display = 'none';
+    console.log(elImg.id);
+    console.log(elImg.src);
+    elEditAreaImg.src = elImg.src;
+    // elEditAreaImg.style.margin = 20 px;
 }
 
-function closeEditMod(elImg){
+function closeEditMod(elImg) {
     var elEditArea = document.querySelector('.edit-area');
-        elEditArea.style.display = 'none';
+    elEditArea.style.display = 'none';
+    var elImgArea = document.querySelector('.imgs-container');
+    elImgArea.style.display = 'block';
 
 }
 
